9e43b04d0409ed85906ee2fb4e2b986fa1f4f1b8
crynobone <crynobone@gmail.com>
2015-02-13 07:27:39 +0800

    Initial commit.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/StudioServiceProvider.php
@@ -0,0 +1,16 @@
+<?php namespace Orchestra\Studio;
+
+use Illuminate\Support\ServiceProvider;
+
+class StudioServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
