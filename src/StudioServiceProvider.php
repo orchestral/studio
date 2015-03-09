1b36b9c4ce451f7459cc0054f4604d6bfd8600f5
crynobone <crynobone@gmail.com>
2015-03-09 20:01:56 +0800

    Add menu make command.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/StudioServiceProvider.php
+++ src/StudioServiceProvider.php
@@ -1,9 +1,26 @@
 <?php namespace Orchestra\Studio;
 
 use Illuminate\Support\ServiceProvider;
+use Orchestra\Studio\Console\MenuMakeCommand;
 
 class StudioServiceProvider extends ServiceProvider
 {
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = true;
+
+    /**
+     * The commands to be registered.
+     *
+     * @var array
+     */
+    protected $commands = [
+        'MenuMake' => 'orchestra.studio.command.menu.make',
+    ];
+
     /**
      * Register the service provider.
      *
@@ -11,6 +28,33 @@ class StudioServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        foreach (array_keys($this->commands) as $command) {
+            $method = "register{$command}Command";
+            call_user_func_array([$this, $method], []);
+        }
+
+        $this->commands(array_values($this->commands));
+    }
+
+    /**
+     * Register the command.
+     *
+     * @return void
+     */
+    protected function registerMenuMakeCommand()
+    {
+        $this->app->singleton($this->commands['MenuMake'], function ($app) {
+            return new MenuMakeCommand($app['files']);
+        });
+    }
+
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array_values($this->commands);
     }
 }
