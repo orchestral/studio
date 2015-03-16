5f4bdd8c0ed5049c10126ff4830dee9e1f2653e4
crynobone <crynobone@gmail.com>
2015-03-16 22:25:12 +0800

    Add filter command.
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ src/Console/FilterMakeCommand.php
@@ -0,0 +1,47 @@
+<?php namespace Orchestra\Studio\Console;
+
+class FilterMakeCommand extends GeneratorCommand
+{
+    /**
+     * The console command name.
+     *
+     * @var string
+     */
+    protected $name = 'make:filter';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = "Create a new Filter class";
+
+    /**
+     * The type of class being generated.
+     *
+     * @var string
+     */
+    protected $type = 'Filter class';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/../../stubs/filter.stub';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param  string  $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Http\Filters';
+    }
+}
