1fe5479ede438a9fc8ec534edea4ac07b977cf96
crynobone <crynobone@gmail.com>
2015-08-23 16:56:43 +0800

    Add password reset.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Console/AuthControllerMakeCommand.php
+++ src/Console/AuthControllerMakeCommand.php
@@ -39,6 +39,7 @@ class AuthControllerMakeCommand extends PublishCommand
             "{$stub}/controllers/auth/authenticate.stub"   => app_path('Http/Controllers/Auth/AuthenticateController.php'),
             "{$stub}/controllers/auth/deauthenticate.stub" => app_path('Http/Controllers/Auth/DeauthenticateController.php'),
             "{$stub}/controllers/auth/register.stub"       => app_path('Http/Controllers/Auth/RegisterController.php'),
+            "{$stub}/controllers/auth/password.stub"       => app_path('Http/Controllers/Auth/PasswordController.php'),
 
             "{$stub}/views/app.blade.php" => base_path('resources/views/app.blade.php'),
             "{$stub}/views/auth"          => base_path('resources/views/auth'),
