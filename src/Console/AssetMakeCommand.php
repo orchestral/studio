be0470744baccc9cf6fed60aecbe5336caa9aee1
crynobone <crynobone@gmail.com>
2016-06-22 08:46:49 +0800

    Add css and fonts publishing.
    Signed-off-by: crynobone <crynobone@gmail.com>

--- src/Console/AssetMakeCommand.php
+++ src/Console/AssetMakeCommand.php
@@ -61,6 +61,8 @@ class AuthMakeCommand extends PublishCommand
             "{$stub}/paths.js"    => base_path('paths.js'),
 
             "{$foundation}/assets/img"    => base_path('resources/assets/img'),
+            "{$foundation}/assets/css"    => base_path('resources/assets/css'),
+            "{$foundation}/assets/fonts"  => base_path('resources/assets/fonts'),
             "{$foundation}/assets/less"   => base_path('resources/assets/less'),
             "{$foundation}/assets/vendor" => base_path('resources/assets/vendor'),
             "{$foundation}/js"            => base_path('resources/js'),
